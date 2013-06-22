9f5993a650e38da96b09abe1ab51e9687f98bac3
Clément Bourgeois <moonpyk@gmail.com>
2013-06-22 20:30:36 +0200

    Ported spruce.

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function (grunt) {
             simplex: {}, slate: {}, spacelab: {}, spruce: {}, superhero: {}, united: {}
         },
         "swatch": {
-            cosmo: {}, cerulean: {}, flatly: {}, united: {}
+            cosmo: {}, cerulean: {}, flatly: {}, spruce:{}, united: {}
         },
         clean: {
             build: {
