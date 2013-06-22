ce002f23ab68a7a243444d781f77a1b506499ad2
Clément Bourgeois <moonpyk@gmail.com>
2013-06-22 21:15:08 +0200

    Ported readable.

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,6 +37,7 @@ module.exports = function (grunt) {
             cyborg: {},
             flatly: {},
             journal: {},
+            readable: {},
             slate: {},
             spancelab: {},
             spruce:{},
